e1695f16060bbe535763f4d168b12e133906cd89
yusufk05 <ykaracin@gmail.com>
2019-12-26 23:58:47 -0500

    day 18

--- src/test/java/tests/vytrack/SmokeTest.java
+++ src/test/java/tests/vytrack/SmokeTest.java
@@ -1,9 +1,28 @@
 package tests.vytrack;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
 import tests.TestBase;
 
 public class SmokeTest extends TestBase {
+
+    @Test(dataProvider = "navigationInfo")
+    public void smokeTest(String moduleName, String subModuleName, String pageSubtitle){
+
+        extentTest = extentReports.createTest("Verify that page subtitle is equals to "+pageSubtitle);
+
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.login("Storemanager85", "UserUser123");
+
+        loginPage.navigateTo(moduleName,subModuleName);
+
+        Assert.assertEquals(loginPage.getPageSubTitle(), pageSubtitle);
+
+        extentTest.pass("Verified that page subtitle '"+pageSubtitle+"' is displayed");
+    }
 
     @DataProvider(name = "navigationInfo")
     public Object[][] navigationInfo(){
@@ -12,7 +31,7 @@ public class SmokeTest extends TestBase {
                 {"Dashboard", "Manage Dashboard", "All Manage Dashboard"},
                 {"Fleet", "Vehicles", "All Cars"},
                 {"Customers", "Accounts", "All Accounts"}
-        }
+        };
     }
 
 }
